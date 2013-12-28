870f3a195288395d2a2d9c5dfbdb107a9d1ffe00
skimrudeen <skimrudeen@gmail.com>
2013-12-28 12:29:19 +0530

    newbank

---
+++ Bank/src/SampleBank.java
@@ -0,0 +1,23 @@
+import bankpa.*;
+public class SampleBank
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		Account accountObject1 = new Account("John Mayor");
+		AccountList.addAccountToList(accountObject1);
+		
+		Account accountObject2 = new Account(5000, "Mary Roberts");
+		AccountList.addAccountToList(accountObject2);
+		
+		ATMOperation atmSession1 =  new ATMOperation(1002);
+		System.out.println(String.format("%d has balance: %d", 1002, atmSession1.getCustomerAccountbalance()));
+		
+		int updatedBalance = atmSession1.withdrawAmount(1000);
+		System.out.println(String.format("%d has balance: %d", 1002, atmSession1.getCustomerAccountbalance()));
+
+	}
+
+}
---
+++ Bank/src/bankpa/ATMOperation.java
@@ -0,0 +1,21 @@
+package bankpa;
+
+public class ATMOperation 
+{
+	private int accountNumber;
+	private Account customerAccount;
+	
+	public ATMOperation(int accountNumber){
+		this.accountNumber = accountNumber;
+		customerAccount = AccountList.getAccountDetails(this.accountNumber);
+	}
+	
+	public int getCustomerAccountbalance(){
+		return customerAccount.getBalance();
+	}
+	
+	public int withdrawAmount(int amount){
+		return customerAccount.withdrawAmount(amount);
+	}
+
+}
